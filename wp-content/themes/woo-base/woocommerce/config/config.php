60ec5baa3878b91b6bc171728cb00562391178eb
Mohamad Rashid <mohtof2000@oddhill.se>
2016-11-09 00:11:41 +0100

    Added autoloader function.

---
+++ wp-content/themes/woo-base/woocommerce/config/config.php
@@ -0,0 +1,14 @@
+<?php //config.php
+spl_autoload_register(function($class) {
+	if (is_file('Classes/' . $class . '.php')) 
+	{
+		require_once('Classes/' . $class . '.php');
+	}else
+	 {
+	 	 $filename = dirname(dirname(__FILE__)) . '/Classes/' . $class .'.php';
+	 	 if(is_readable($filename))
+	 	 {
+	 	 	require_once $filename;
+	 	 }
+	 }
+});
