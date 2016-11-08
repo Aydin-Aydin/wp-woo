1bc0ea776c3b7593cd9d3db7b89f5ac4e399303b
Mohamad Rashid <mohtof2000@oddhill.se>
2016-11-08 20:35:07 +0100

    Modified packageJson

--- wp-content/themes/woo-base/gulpfile.js
+++ wp-content/themes/woo-base/gulpfile.js
@@ -1,48 +1,91 @@
-var gulp = require('gulp');
-var livereload = require('gulp-livereload')
-var uglify = require('gulp-uglifyjs');
-var sass = require('gulp-sass');
-var autoprefixer = require('gulp-autoprefixer');
-var sourcemaps = require('gulp-sourcemaps');
-var imagemin = require('gulp-imagemin');
-var pngquant = require('imagemin-pngquant');
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const eslint = require('gulp-eslint');
+const scsslint = require('gulp-scss-lint');
+const svg2png = require('gulp-svg2png');
+const browserify = require('browserify');
+const shim = require('browserify-shim');
+const babelify = require('babelify');
+const cssGlobbing = require('gulp-css-globbing');
+const source = require('vinyl-source-stream');
+const gutil = require('gulp-util');
+const watchify = require('watchify');
 
-
-
-
-gulp.task('imagemin', function () {
-    return gulp.src('./wp-content/themes/woo-base/images/*')
-        .pipe(imagemin({
-            progressive: true,
-            svgoPlugins: [{removeViewBox: false}],
-            use: [pngquant()]
-        }))
-        .pipe(gulp.dest('./wp-content/themes/woo-base/images'));
+// Browserify
+const b = browserify({
+  basedir: './js',
+  entries: './main.js',
+  cache: {},
+  packageCache: {}
 });
 
+b.transform(babelify, { presets: ['es2015'] });
+b.transform(shim, { global: true });
 
+// Bundle Browserify
+function bundle() {
+  return b.bundle()
+    .on('error', function (err) {
+      gutil.log(gutil.colors.red('Browserify build error:\n') + err.message);
+    })
+    .pipe(source('bundle.js'))
+    .pipe(gulp.dest('./dist/js'));
+}
+
+// Browserify gulp task
+gulp.task('browserify', function () {
+  return bundle();
+});
+
+// svg2png
+gulp.task('svg2png', function () {
+  gulp.src('./graphics/**/*.svg')
+    .pipe(svg2png())
+    .pipe(gulp.dest('./graphics'));
+});
+
+// Compile sass
 gulp.task('sass', function () {
-  gulp.src('./wp-content/themes/woo-base/sass/**/*.scss')
-    .pipe(sourcemaps.init())
-        .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
-        .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 7', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
-    .pipe(sourcemaps.write('./'))
-    .pipe(gulp.dest('./wp-content/themes/woo-base'));
+  return gulp.src(['./scss/main.scss'])
+    .pipe(cssGlobbing({
+      extensions: ['.scss']
+    }))
+    .pipe(sass({
+      includePaths: [
+        'node_modules/bourbon/app/assets/stylesheets/',
+        'node_modules/bourbon-neat/app/assets/stylesheets/',
+        'node_modules/normalize.css/',
+        'node_modules/Stratagem/'
+      ]
+    }).on('error', sass.logError))
+    .pipe(gulp.dest('./dist/css'));
 });
 
-
-gulp.task('uglify', function() {
-  gulp.src('./wp-content/themes/woo-base/lib/*.js')
-    .pipe(uglify('woo-base.min.js'))
-    .pipe(gulp.dest('./wp-content/themes/woo-base/js'))
+// Lint sass
+gulp.task('scss-lint', function () {
+  return gulp.src(['./scss/**/*.scss', '!./scss/print.scss', '!scss/normalize.scss', '!scss/vendor/**/*'])
+    .pipe(scsslint());
 });
 
-gulp.task('watch', function(){
-    livereload.listen();
-
-    gulp.watch('./wp-content/themes/woo-base/sass/**/*.scss', ['sass']);
-    gulp.watch('./wp-content/themes/woo-base/lib/*.js', ['uglify']);
-    gulp.watch(['./wp-content/themes/woo-base/style.css', './wp-content/themes/woo-base/*.php', './wp-content/themes/woo-base/js/*.js', './wp-content/themes/woo-base/parts/**/*.php'], function (files){
-        livereload.changed(files)
-    });
+// Eslint
+gulp.task('eslint', function () {
+  return gulp.src('./js/**/*.js')
+    .pipe(eslint())
+    .pipe(eslint.format());
 });
+
+// Watch .scss and .js
+gulp.task('watch', function () {
+  gulp.watch('scss/**/*.scss', ['scss-lint', 'sass']);
+  gulp.watch('js/**/*.js', ['eslint']);
+
+  b.plugin(watchify);
+  b.on('update', bundle);
+  b.on('log', function (msg) {
+    gutil.log('Browserify build - ' + msg);
+  });
+  bundle();
+});
+
+// Set default task
+gulp.task('default', ['watch']);
