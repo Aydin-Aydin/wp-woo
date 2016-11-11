28b844890a28c8e34afa370bdc0fb7efa9a9a358
Aydin Aydin <aydin@handkontroll.com>
2016-11-11 10:57:42 +0100

    Just testting the index file

--- wp-content/themes/woo-base/index.php
+++ wp-content/themes/woo-base/index.php
@@ -1,9 +1,10 @@
 <?php get_header(); ?>
 
-	<h1>Welcome to woo-base theme.</h1>
+	<h1>Welcome to Aydin.</h1>
     <p>
         You can delete this text in index.php
     </p>
+<?php require( dirname( __FILE__ ) . '/wp-blog-header.php' ); ?>
+<?php get_sidebar('SidebarOne'); ?>
 
-    
 <?php get_footer(); ?>
