80863ee35d5b24c338ff1d28ab9e78cff6565832
Mohamad Rashid <mohtof2000@oddhill.se>
2016-11-12 20:19:49 +0100

    Created masonry grid.

--- wp-content/themes/woo-base/functions.php
+++ wp-content/themes/woo-base/functions.php
@@ -3,7 +3,7 @@
 /*  Register Scripts and Style */
 /*  Register Scripts and Style */
 function theme_register_scripts() {
-    wp_enqueue_style( 'woo-base-css', get_stylesheet_directory_uri() . '/css/style.css');
+    wp_enqueue_style( 'woo-base-css', get_stylesheet_directory_uri() . '/dist/css/style.css');
     wp_enqueue_script( 'woo-base-js', esc_url( trailingslashit( get_template_directory_uri() ) . 'js/woo-base.min.js' ), array( 'jquery' ), '1.0', true );
 
     wp_enqueue_script( 'main-js', esc_url( trailingslashit( get_template_directory_uri() ) . 'lib/main.js' ));
--- wp-content/themes/woo-base/lib/custom/masonry-grid.js
+++ wp-content/themes/woo-base/lib/custom/masonry-grid.js
@@ -1,7 +1,14 @@
 import $ from 'jquery';
+import jQueryBridget from 'jquery-bridget';
+import Masonry from 'masonry-layout';
+jQueryBridget('masonry', Masonry, $);
 const masonGrid = {
 	init() {
-		
+		$('.grid').masonry({
+			itemSelector: '.grid-item',
+			columnWidth: '.grid-sizer',
+			precentPosition: true
+		});
 	}
 };
 export default masonGrid;
