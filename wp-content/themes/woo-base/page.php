0bf19fbaaabf9a9dbe388cfa2a243c4879611859
Pontus Kihlberg <pontuskihlberg@Pontuss-MacBook-Pro.local>
2016-11-15 13:45:15 +0100

    Created Slider Post Type (Front page)

--- wp-content/themes/woo-base/page.php
+++ wp-content/themes/woo-base/page.php
@@ -1,4 +1,22 @@
 <?php get_header(); ?>
 	<?php get_sidebar('sidebar-first'); ?>
-<h1>ölkajfödajdk</h1>
+<?php
+  $args = ['post_type' => 'slider'];
+  $loop = new WP_Query( $args );
+
+  while ( $loop->have_posts() ) : $loop->the_post(); ?>
+  <div class="slider-wrapper" style="background-image: url(<?php the_field("front_slider_img"); ?>);">
+    <h1><?php the_title(); ?></h1>
+
+    <div class="entry-content">
+
+      <?php the_content(); ?>
+      <a href="<?php the_field('front_slider_link');?>">Click</a>
+    </div>
+  </div>
+
+
+
+  <?php endwhile; ?>
+
 <?php get_footer(); ?>
