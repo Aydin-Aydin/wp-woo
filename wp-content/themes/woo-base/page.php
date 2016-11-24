69fc2bf05db619351f50b4d36a09ccaaf0748956
Mohamad Rashid <mohtof2000@oddhill.se>
2016-11-24 23:02:33 +0100

    Resolved conflict page.php

--- wp-content/themes/woo-base/page.php
+++ wp-content/themes/woo-base/page.php
@@ -1,7 +1,7 @@
 <?php get_header(); ?>
-<<<<<<< HEAD
 	<?php get_sidebar('sidebar-first'); ?>
 <section class="primary-wrapper">
+<?php if (is_front_page()): ?>
   <div class="slider-wrapper woo-main-content">
     <?php
       $args = ['post_type' => 'slider'];
@@ -26,41 +26,16 @@
 
 
     <?php endwhile; ?>
-  </div>
-
-  <div class="front-widget">
-    <?php dynamic_sidebar( 'top-rated' ); ?>
-  </div>
-</section>
-=======
-  <?php get_sidebar('sidebar-first'); ?>
-
-<div class="slider-wrapper woo-content"> 
-  <?php if (is_front_page()): ?>
-    <?php
-      $args = ['post_type' => 'slider'];
-      $loop = new WP_Query( $args );
-
-      while ( $loop->have_posts() ) : $loop->the_post();
-    ?>
-      <div class="slider-content" style="background-image: url(<?php the_field("front_slider_img"); ?>);">
-        <div class="entry-content">
-          <div class="overlay">
-
-            <h1><?php the_title(); ?></h1>
-            <?php the_content(); ?>
-            <a href="<?php the_field('front_slider_link');?>">Click</a>
-
-          </div>
-        </div>
-      </div>
-    <?php endwhile; ?>
-  <?php endif ?>
+  </div><!-- end slider-wrapper woo-main-content -->
+<?php endif ?>
 <?php if (have_posts): ?>
   <?php while (have_posts()): the_post(); ?>
     <?php the_content(); ?>
     <?php endwhile; ?>
 <?php endif ?>
-</div><!-- end slider-wrapper woo-content -->
->>>>>>> mohamad
+
+  <div class="front-widget">
+    <?php dynamic_sidebar( 'top-rated' ); ?>
+  </div>
+</section>
 <?php get_footer(); ?>
