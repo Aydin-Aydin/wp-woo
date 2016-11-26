5f3be5c95e50a29378d471fdfb9ab803c5417412
Mohamad Rashid <mohtof2000@oddhill.se>
2016-11-26 18:00:39 +0100

    Created cart template.

--- wp-content/themes/woo-base/page.php
+++ wp-content/themes/woo-base/page.php
@@ -22,10 +22,5 @@
       </div>
     <?php endwhile; ?>
   <?php endif ?>
-<?php if (have_posts): ?>
-  <?php while (have_posts()): the_post(); ?>
-    <?php the_content(); ?>
-    <?php endwhile; ?>
-<?php endif ?>
 </div><!-- end slider-wrapper woo-content -->
 <?php get_footer(); ?>
