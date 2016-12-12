e5c67067fc6e52f07c7b2ebcd99902bad7134665
mussealani <mohamad.rashid@oddhill.se>
2016-12-12 10:43:52 +0100

    Modified comment section.

--- wp-content/themes/woo-base/lib/main.js
+++ wp-content/themes/woo-base/lib/main.js
@@ -28,6 +28,7 @@ import containerMargin from './custom/container-margin';
     });
 
 
+<<<<<<< Updated upstream
     // You may also like section
     $('.summary-tabs-wrapper .grid-sizer').css({
       width: "45%"
@@ -60,12 +61,41 @@ import containerMargin from './custom/container-margin';
     $('.equal-height').matchHeight(options);
     $('.variations select').niceSelect();
 
+=======
+    // Options
+    var options = {
+      max_value: 5,
+      step_size: 0.5,
+      initial_value: 0,
+      selected_symbol_type: 'utf8_star', // Must be a key from symbols
+      cursor: 'default',
+      readonly: true,
+    }
+
+    $(".rating").rate(options);
+
+    $(".rating").rate("getValue");
+
+    options = {
+      byRow: true,
+      property: 'height',
+      target: null,
+      remove: false
+    };
+
+
+    //comment-form-rating
+
+    $('.equal-height').matchHeight(options);
+    $('.variations select').niceSelect();
+>>>>>>> Stashed changes
 
     $('.value .reset_variations').css({
       position: 'absolute',
       zIndex: '-10'
     });
 
+<<<<<<< Updated upstream
     // equal-height
     // $("*").each(function() {
     //     if ($(this).width() > $(window).width()) {
@@ -86,6 +116,11 @@ import containerMargin from './custom/container-margin';
     // You may also like section
     $('.up-sells .grid-sizer').addClass('grid-sizer-2-columns').removeClass('grid-sizer');
 
+=======
+    // You may also like section
+    $('.up-sells .grid-sizer').addClass('grid-sizer-2-columns').removeClass('grid-sizer');
+
+>>>>>>> Stashed changes
 
     $('.up-sells .grid-item').addClass('grid-item-2-columns').removeClass('grid-item');
     //$('.summary-tabs-wrapper .products .grid-item').css({ left: $('.summary-tabs-wrapper .products .grid-item').width()});
