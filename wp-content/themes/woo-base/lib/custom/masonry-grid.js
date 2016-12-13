5a9419b2700f4d01eda886f11c1e5798984cd3e9
Mohamad Rashid <mohtof2000@oddhill.se>
2016-12-14 00:03:55 +0100

    Modified grid system.

--- wp-content/themes/woo-base/lib/custom/masonry-grid.js
+++ wp-content/themes/woo-base/lib/custom/masonry-grid.js
@@ -4,6 +4,13 @@ import Masonry from 'masonry-layout';
 jQueryBridget('masonry', Masonry, $);
 const masonGrid = {
 	init() {
+		// Grid 2 columns
+		$('.up-sells .grid-sizer').addClass('grid-sizer-2-columns').removeClass('grid-sizer');
+		$('.up-sells .grid-item').addClass('grid-item-2-columns').removeClass('grid-item');
+
+		// Latest product widget
+		$('#woocommerce_products-2 ul').addClass('grid').prepend('<div class="grid-sizer"></div><div class="gutter-sizer"></div>').find('li').addClass('grid-item');
+
 		// You may also like section
 		$('.grid').masonry({
 			itemSelector: '.grid-item-2-columns',
@@ -19,12 +26,6 @@ const masonGrid = {
 			gutter: '.gutter-sizer',
 			percentPosition: true
 		});
-		// Grid 2 columns
-	  $('.up-sells .grid-sizer').addClass('grid-sizer-2-columns').removeClass('grid-sizer');
-	  $('.up-sells .grid-item').addClass('grid-item-2-columns').removeClass('grid-item');
-
-// Latest product widget
-$('#woocommerce_products-2 ul').addClass('grid').prepend('<div class="grid-sizer"></div><div class="gutter-sizer"></div>').find('li').addClass('grid-item');	  
 	}
 };
 export default masonGrid;
