333b44c89dc9c621c8792d669ee41dd389ff358c
Sergio Follyvi <sergiofollyvi@hotmail.com>
2016-12-15 13:34:59 +0100

    fixed primary-wrapper leftpadding

--- wp-content/themes/woo-base/lib/custom/container-margin.js
+++ wp-content/themes/woo-base/lib/custom/container-margin.js
@@ -1,13 +1,10 @@
 import $ from 'jquery';
 const containerMargin = {
   init() {
-    $('.sidebar-left-container').css({
-      width: $('#left-sidebar').outerWidth() + 'px'
-    });
-    $('.primary-wrapper').css({
-      paddingLeft: $('.sidebar-left-container').outerWidth() + 'px'
-    });
-    console.log($('.sidebar-left-container').outerWidth());
+
+      $('.primary-wrapper').css({
+        paddingLeft: $('.sidebar-left-container').outerWidth() + 'px'
+      });
   }
 };
 export default containerMargin;
