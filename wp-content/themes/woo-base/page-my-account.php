8802e2b5a17d94cf14e31ae7f7310e974bf10497
Pontus Kihlberg <pontuskihlberg@gmail.com>
2016-12-18 17:08:33 +0100

    Added my account

--- wp-content/themes/woo-base/page-my-account.php
+++ wp-content/themes/woo-base/page-my-account.php
@@ -1,15 +1,17 @@
 <?php get_header(); ?>
   <?php get_sidebar('sidebar-first'); ?>
-<section class="primary-wrapper my-account">
-  <div class="woo-main-content">
-    <div id="cssanimation">
-      <div id="animationbox">
-    <?php if(have_posts()): while(have_posts()): the_post() ?>
-      <?php the_content(); ?>
-    <?php endwhile; endif; ?>
-    </div>
-    </div>
-  </div><!-- end slider-wrapper woo-main-content -->
+<section class="primary-wrapper">
+  <div id="my-account">
+    <div class="woo-main-content">
+      <div id="cssanimation">
+        <div id="animationbox">
+      <?php if(have_posts()): while(have_posts()): the_post() ?>
+        <?php the_content(); ?>
+      <?php endwhile; endif; ?>
+      </div>
+      </div>
+    </div><!-- end slider-wrapper woo-main-content -->
+  </div>
 </section>
 <?php get_sidebar(); ?>
 <?php get_footer(); ?>
